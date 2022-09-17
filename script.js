17875233385314e8d044de189cbd540446a57f2a
Lincoln <lincoln@taylorlivingco.com>
2022-09-17 18:29:58 +1000

    second

---
+++ script.js
@@ -0,0 +1,38 @@
+
+
+function randomMeal() {
+  fetch("https://www.themealdb.com/api/json/v1/1/random.php")
+    .then((response) => response.json())
+    // {
+    //   var test = response.json();
+    //   console.log(test);
+    // }
+    .then((data) => {
+      const meal = data.meals[0];
+      const div = document.querySelector("div.random-meal");
+      const img = div.querySelector("img");
+      img.src = meal.strMealThumb;
+      const h2 = div.querySelector("h2");
+      h2.innerHTML = meal.strMeal;
+
+      const ingredients = document.querySelector("div.ingredients");
+      ingredients.innerHTML = "<h3>Ingredients</h3>";
+      const unorderedList = document.createElement("ul");
+      for (const [key, value] of Object.entries(meal)) {
+        if (key.includes("strIngredient") && !(!value || value.length === 0)) {
+          const ingredient = document.createElement("li");
+          ingredient.innerHTML = value;
+          unorderedList.appendChild(ingredient);
+        }
+      }
+      ingredients.appendChild(unorderedList);
+
+      const instructions = document.querySelector("div.instructions");
+      instructions.innerHTML = "<h3>Instructions</h3>";
+      const instruction = document.createElement("p");
+      instruction.innerHTML = meal.strInstructions;
+      instructions.appendChild(instruction);
+    });
+}
+
+
